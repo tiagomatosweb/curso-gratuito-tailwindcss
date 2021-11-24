ee80863fb0d2f1cb268b78cfbb8f8269d9f9ecaa
Tiago Matos <tiago@tiagomatos.com>
2021-11-24 18:18:48 +1100

    Aula 14

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 module.exports = {
   purge: [],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: 'class',
   theme: {
     extend: {
-      colors: {
-        brand: {
-          100: '#c542f5',
-          200: '#9e35c4',
-          300: '#6d2487',
-          400: '#331040',
-        }
-      }
     },
   },
   variants: {
